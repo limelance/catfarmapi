9f63e14d9391e6a3f88c485f6441e61ce39da6af
Peter Lipkin <hf.nilor@gmail.com>
2017-10-03 21:47:17 +0300

    deployment preparations

--- CatAPI/wsgi.py
+++ CatAPI/wsgi.py
@@ -8,10 +8,17 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import dotenv
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CatAPI.settings")
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 application = DjangoWhiteNoise(get_wsgi_application())
